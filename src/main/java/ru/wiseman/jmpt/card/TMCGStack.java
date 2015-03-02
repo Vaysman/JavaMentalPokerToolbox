79ba67181d3f9540de18bff4e0b713b8047b3298
Mikhail Vaysman <mikhail.vaysman@gmail.com>
2015-03-02 22:34:17 +0300

    add stack

---
+++ src/main/java/ru/wiseman/jmpt/card/TMCGStack.java
@@ -0,0 +1,9 @@
+package ru.wiseman.jmpt.card;
+
+import java.util.List;
+
+public class TMCGStack {
+    private List<Card> stack;
+
+
+}
--- src/main/java/ru/wiseman/jmpt/stack/StackSecret.java
+++
@@ -1,4 +0,0 @@
-package ru.wiseman.jmpt.stack;
-
-public interface StackSecret {
-}
